c255c1442f855af193368e2a15c628d6dfb8bf85
Manasseh-Ankrah <solomonankrah24@gmail.com>
2021-12-03 20:13:28 +0000

    third commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,14 @@ import "./Home.css";
 import ParticlesBg from "particles-bg";
 import { Button } from "@material-ui/core";
 
+import img from "../images/team-1.jpg";
+
 function Home() {
   return (
     <div className="home">
       <Header />
       <div className="home__animation">
-        <ParticlesBg
-          className="home__animation__particle"
-          // height="80vh"
-          type="cobweb"
-        />
+        <ParticlesBg className="home__animation__particle" type="cobweb" />
       </div>
 
       <div className="home__text">
@@ -35,12 +33,30 @@ function Home() {
       </div>
 
       <div className="home__section">
-        <div className="home__section__image">hello</div>
+        <div className="home__section__image">
+          <img className="home__img" src={img} />
+        </div>
         <div className="home__section__text">
-          <h2>Hello World</h2>
-          <p>Once upon a time, there was a boy named manasseh</p>
+          <h2>We create products not just art</h2>
+          <p>
+            My name is John Smith. I am a web designer and developer.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
+            ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel
+            facilisis.
+          </p>
+
+          <Button className="home__btn__about" variant="contained" size="large">
+            About
+          </Button>
         </div>
       </div>
+
+      {/* dsdfd */}
+
+      {/* <div className="home__section">
+        <p style={{ color: "white" }}>Hello world</p>
+      </div> */}
     </div>
   );
 }
